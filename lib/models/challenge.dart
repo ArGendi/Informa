f7af7ab6669bf22e2c1d3787436330744286d06e
ArGendi <73427247+ArGendi@users.noreply.github.com>
2021-12-20 20:33:09 +0200

    challenge model

--- lib/models/challenge.dart
+++ lib/models/challenge.dart
@@ -1,8 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Challenge {
-  int? id;
+  String? id;
   String? name;
+  String? description;
   DateTime? deadline;
   String? url;
+  Map<String, dynamic>? submits;
 
-  Challenge({this.id, this.name, this.url, this.deadline});
+  Challenge({this.id, this.name, this.url, this.deadline, this.description});
+
+  fromJson(Map<String, dynamic> json){
+    name = json['name'];
+    description = json['description'];
+    var tsDeadline = json['deadline'] as Timestamp;
+    deadline = tsDeadline.toDate();
+    url = json['url'];
+    submits = json['submits'];
+  }
 }
