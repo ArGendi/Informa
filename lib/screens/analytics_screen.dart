a0b1c0b16a8d7514248f81de959d398334de9e04
HhmedHesham <heshama7793@gmail.com>
2022-08-07 22:28:57 +0200

    add localization to the analytrics screen

--- lib/screens/analytics_screen.dart
+++ lib/screens/analytics_screen.dart
@@ -5,6 +5,8 @@ import 'package:informa/models/progress.dart';
 import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
+import '../app_localization.dart';
+
 class AnalyticsScreen extends StatefulWidget {
   const AnalyticsScreen({Key? key}) : super(key: key);
 
@@ -15,6 +17,8 @@ class AnalyticsScreen extends StatefulWidget {
 class _AnalyticsScreenState extends State<AnalyticsScreen> {
   @override
   Widget build(BuildContext context) {
+    var localization = AppLocalization.of(context);
+
     final List<Progress> chartData = [
       Progress(date: DateTime.now(), wieght: 80),
       Progress(date: DateTime(2019), wieght: 50),
@@ -66,7 +70,9 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                       ),
                     ),
                     subtitle: Text(
-                      'تقدر تشوف وزنك على مدار الفترات الزمنية',
+                      localization!
+                          .translate('تقدر تشوف وزنك على مدار الفترات الزمنية')
+                          .toString(),
                       style: TextStyle(fontSize: 12),
                     ),
                   ),
@@ -74,7 +80,10 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                   Container(
                     child: SfCartesianChart(
                       primaryXAxis: CategoryAxis(),
-                      title: ChartTitle(text: 'التقدم الحالي'),
+                      title: ChartTitle(
+                          text: localization
+                              .translate('التقدم الحالي')
+                              .toString()),
                       tooltipBehavior: TooltipBehavior(enable: true),
                       series: <ChartSeries>[
                         LineSeries<Progress, String>(
@@ -83,7 +92,7 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                                 DateFormat.yMd().format(progress.date),
                             yValueMapper: (Progress progress, _) =>
                                 progress.wieght,
-                            name: 'الوزن',
+                            name: localization.translate('الوزن').toString(),
                             dataLabelSettings:
                                 DataLabelSettings(isVisible: true)
                             // Enable data label
@@ -125,11 +134,14 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                         ),
                       ),
                       title: Text(
-                        'نسبة الدهون',
+                        localization.translate('نسبة الدهون').toString(),
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                       subtitle: Text(
-                        'تقدر تشوف نسبة دهونك على مدار الفترات الزمنية',
+                        localization
+                            .translate(
+                                'تقدر تشوف نسبة دهونك على مدار الفترات الزمنية')
+                            .toString(),
                         style: TextStyle(fontSize: 12),
                       ),
                     ),
@@ -140,16 +152,17 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                         tooltipBehavior: TooltipBehavior(enable: true),
                         series: <ChartSeries>[
                           LineSeries<Progress, String>(
-                              dataSource: chartData,
-                              xValueMapper: (Progress progress, _) =>
-                                  DateFormat.yMd().format(progress.date),
-                              yValueMapper: (Progress progress, _) =>
-                                  progress.wieght,
-                              name: 'الدهون',
-                              dataLabelSettings:
-                                  DataLabelSettings(isVisible: true)
-                              // Enable data label
-                              ),
+                            dataSource: chartData,
+                            xValueMapper: (Progress progress, _) =>
+                                DateFormat.yMd().format(progress.date),
+                            yValueMapper: (Progress progress, _) =>
+                                progress.wieght,
+                            name: 'الدهون',
+                            dataLabelSettings:
+                                DataLabelSettings(isVisible: true),
+
+                            // Enable data label
+                          ),
                         ],
                       ),
                     ),
@@ -159,107 +172,147 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
               SizedBox(
                 height: 20,
               ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(1, 2),
-                        blurRadius: 1),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    ListTile(
-                      trailing: Icon(Icons.arrow_back_ios_new),
-                      leading: Container(
-                        width: 35,
-                        height: 35,
-                        child: SvgPicture.asset(
-                          'assets/icons/body_mesurements.svg',
-                        ),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 4, vertical: 5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: primaryColor,
-                        ),
-                      ),
-                      title: Text(
-                        'قياسات الجسم',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        'تقدر تشوف قياسات الجسم على مدار الفترات الزمنية',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                    Divider(),
-                    Row(
-                      children: [
-                        Spacer(flex: 1),
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.red,
-                              radius: 8,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              DateFormat.yMMMMd().format(DateTime(2021)),
-                              style: TextStyle(fontSize: 14),
-                            ),
-                          ],
-                        ),
-                        Spacer(flex: 8),
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.green,
-                              radius: 8,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              DateFormat.yMMMMd().format(DateTime.now()),
-                              style: TextStyle(fontSize: 14),
-                            ),
-                          ],
-                        ),
-                        Spacer(flex: 1),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            BodyLabel(bodyOrgan: 'الرقبة', bodyOrganValue: 13),
-                          ],
-                        ),
-                        Expanded(
-                            child:
-                                Image.asset('assets/images/body_measure.JPG')),
-                        Column(
-                          children: [
-                            BodyLabel(bodyOrgan: 'الرقبة', bodyOrganValue: 13),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
+              BodyMeasurementCard(),
               SizedBox(
                 height: 75,
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class BodyMeasurementCard extends StatelessWidget {
+  const BodyMeasurementCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var localization = AppLocalization.of(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(color: Colors.grey, offset: Offset(1, 2), blurRadius: 1),
+        ],
+      ),
+      child: Column(
+        children: [
+          ListTile(
+            trailing: Icon(Icons.arrow_back_ios_new),
+            leading: Container(
+              width: 35,
+              height: 35,
+              child: SvgPicture.asset(
+                'assets/icons/body_mesurements.svg',
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 4, vertical: 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: primaryColor,
+              ),
+            ),
+            title: Text(
+              localization!.translate('قياسات الجسم').toString(),
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              localization
+                  .translate('تقدر تشوف قياسات الجسم على مدار الفترات الزمنية')
+                  .toString(),
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
+          Divider(),
+          Row(
+            children: [
+              Spacer(flex: 1),
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.red,
+                    radius: 8,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    DateFormat.yMMMMd().format(DateTime(2021)),
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+              Spacer(flex: 8),
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.green,
+                    radius: 8,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    DateFormat.yMMMMd().format(DateTime.now()),
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+              Spacer(flex: 1),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    BodyLabel(bodyOrgan: 'الرقبة', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'البطن', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'يد يسري', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'يد يمني', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'خصر', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'اسفل بطن', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'رجل يمني', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'رجل يسري', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'سمانة يمني', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'سمانة يسري', bodyOrganValue: 13),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/body_measure.JPG'),
+                    SizedBox(
+                      height: 5,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    BodyLabel(bodyOrgan: 'الرقبة', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'البطن', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'يد يسري', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'يد يمني', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'خصر', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'اسفل بطن', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'رجل يمني', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'رجل يسري', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'سمانة يمني', bodyOrganValue: 13),
+                    BodyLabel(bodyOrgan: 'سمانة يسري', bodyOrganValue: 13),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -275,29 +328,44 @@ class BodyLabel extends StatelessWidget {
   final int bodyOrganValue;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(bodyOrgan),
-        SizedBox(
-          width: 20,
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          decoration: BoxDecoration(
-            color: primaryColor,
-            borderRadius: BorderRadius.circular(5),
-            border: Border.all(
-              color: Colors.brown,
+    var localization = AppLocalization.of(context);
+
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: Text(
+              localization!.translate(bodyOrgan).toString(),
+              style: TextStyle(fontSize: 12),
             ),
           ),
-          child: Text(
-            '$bodyOrganValue CM',
-            style: TextStyle(
-              color: Colors.white,
+          SizedBox(
+            width: 2,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 4),
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(
+                  color: Colors.brown,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  '$bodyOrganValue CM',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
